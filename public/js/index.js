59bb5c4dbfa8514aedffd50ac03e0ed6eaa52085
rls5113 <stuartrl@comcast.net>
2019-06-06 15:31:13 -0500

    work on notes, saves dupe

--- public/js/index.js
+++ public/js/index.js
@@ -1,4 +1,42 @@
 $(document).ready(function () {
+    $(".delete-btn").click(function (event) {
+        event.preventDefault();
+        const id = $(this).attr("data");
+        $.ajax(`/remove/${id}`, {
+            type: "PUT"
+        }).then(function(){
+            location.reload();
+        })
+    });
+
+       // event handler for opening the note modal
+       $(".note-btn").click(function (event) {
+        event.preventDefault();
+        console.log(event);
+        const id = $(this).attr("data");
+        console.log(this);
+        $("#article-id").text(id);
+        $("#save-note").attr("data", id);
+        $.ajax(`/articles/${id}`, {
+            type: "GET"
+        }).then(function (article) {
+            console.log("Article: "+article);
+            $(".articles-available").empty();
+            if (article.note[0].length > 0){
+                article.note[0].forEach(v => {
+                  console.log("Note: "+ v);
+                    $(".articles-available").append($(`<li class="list-group-item">${v.text}<button type="button" class="btn btn-danger btn-sm float-right btn-deletenote" data="${v._id}">X</button></li>`));
+                })
+            }
+            else {
+                $(".articles-available").append($(`<li class="list-group-item">No notes for this article yet</li>`));
+                console.log("No notes");
+            }
+        })
+        $("#note-modal").modal("toggle");
+    });
+
+
 $(".save-article-btn").click(function(event) {
    console.log("in save article");
   event.preventDefault();
@@ -19,42 +57,11 @@ $(".save-article-btn").click(function(event) {
   );
 });
 
-$(".delete-btn").click(function (event) {
-  event.preventDefault();
-  const id = $(this).attr("id");
-  $.ajax(`/remove/${id}`, {
-      type: "PUT"
-  }).then(function(){
-      location.reload();
-  })
-});
 
-    // event handler for opening the note modal
-    $(".note-btn").click(function (event) {
-      event.preventDefault();
-      const id = $(this).attr("data");
-      $('#article-id').text(id);
-      $('#save-note').attr('data', id);
-      $.ajax(`/articles/${id}`, {
-          type: "GET"
-      }).then(function (article) {
-          console.log(article);
-          $('.articles-available').empty();
-          if (article[0].note.length > 0){
-              article[0].note.forEach(v => {
-                console.log(v);
-                  $('.articles-available').append($(`<li class='list-group-item'>${v.text}<button type='button' class='btn btn-danger btn-sm float-right btn-deletenote' data='${v._id}'>X</button></li>`));
-              })
-          }
-          else {
-              $('.articles-available').append($(`<li class='list-group-item'>No notes for this article yet</li>`));
-              console.log("No notes");
-          }
-      })
-      $('#note-modal').modal('toggle');
-  });
 
-  $(document).on('click', '.btn-deletenote', function (){
+ 
+
+  $(document).on("click", ".btn-deletenote", function (){
     event.preventDefault();
     // console.log($(this).attr("data"))
     const id = $(this).attr("data");
@@ -62,23 +69,23 @@ $(".delete-btn").click(function (event) {
     $.ajax(`/note/${id}`, {
         type: "DELETE"
     }).then(function () {
-        $('#note-modal').modal('toggle');
+        $("#note-modal").modal("toggle");
     });
 });
 
 
 $("#save-note").click(function (event) {
     event.preventDefault();
-    const id = $(this).attr('data');
-    const noteText = $('#note-input').val().trim();
-    $('#note-input').val('');
+    const id = $(this).attr("data");
+    const noteText = $("#note-input").val().trim();
+    $("#note-input").val("");
     $.ajax(`/note/${id}`, {
         type: "POST",
         data: { text: noteText}
     }).then(function (data) {
         console.log(data)
     })
-    $('#note-modal').modal('toggle');
+    $("#note-modal").modal("toggle");
 });
 
 });
